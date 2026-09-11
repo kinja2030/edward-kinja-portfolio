e8958782a35c2bdea51357c060b4df75bcdc18fa
kinja2030 <edwardkinja3@gmail.com>
2026-09-11 16:12:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll('.nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+  });
+});
